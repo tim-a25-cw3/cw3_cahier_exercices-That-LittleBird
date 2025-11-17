2e53e92db8106d6c8013bc9020d59ee5719fa17e
That-LittleBird <alexia.daniela07@gmail.com>
2025-11-17 16:44:18 -0500

    WOoooo

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,5 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
@@ -7,6 +8,18 @@ class Main {
 
   init() {
     Icons.load();
+    this.initSwiperPagination();
+  }
+
+  initSwiperPagination() {
+    const target = document.querySelector('.js-swiper-pagination');
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
